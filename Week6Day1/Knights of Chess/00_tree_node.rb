76ffb69d8af9ebc0de843776d66534e5fb46ab36
Tasunke Witka <tmbrasfield01@gmail.com>
2019-11-19 13:12:41 -0600

    build tree

---
+++ Week6Day1/Knights of Chess/00_tree_node.rb
@@ -0,0 +1,130 @@
+
+require "byebug"
+
+class PolyTreeNode
+    attr_reader :parent, :value
+    attr_accessor :children
+
+
+    def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+    end
+
+    def parent=(parent_node)
+        if self.parent
+            #self.parent.remove_child(self)
+            self.parent.children.delete(self)
+        end
+        @parent = parent_node #??
+        if parent_node
+            unless parent_node.children.include?(self)
+                parent_node.children << self
+            end
+        end
+        if parent_node.nil?
+            return nil
+        end
+        #parent_node.add_child(self) unless parent_node.children.include?(self)
+    end
+
+    def add_child(child_node)
+        unless self.children.include?(child_node)
+            @children << child_node
+            child_node.parent=(self)
+        end
+    end
+
+    def remove_child(child_node)
+        raise "Node is an Orphan" unless child_node.parent
+        #child_node.parent=(nil)
+        #debugger
+        new_kids = []
+        #debugger
+        @children.each_with_index do |child, i|
+            #debugger
+            unless child == child_node
+                # self.children.delete_at(i)
+                # puts 'hi'
+                new_kids << child
+            end
+        end
+        self.children = new_kids
+        
+        unless child_node.parent.nil?
+            child_node.parent = nil
+        end
+    end
+
+    def inspect
+        @value.inspect
+    end
+
+    def dfs(value)
+        return self if self.value == value
+
+        children.each do |child|
+            bob = child.dfs(value)
+            return bob if bob
+        end
+        nil
+    end
+
+    def bfs(value)
+
+        
+        new_q = []
+
+        new_q << self
+
+        until new_q.empty?
+            current_node = new_q.shift
+            return current_node if current_node.value == value
+            current_node.children.each do |child|
+                new_q << child
+            end
+            
+        end
+        nil
+
+        # if self.value == value
+        #     return self
+        # end
+    end
+
+end
+
+# Nu_Q = Queue.new
+# p Nu_Q
+
+# a = [1,2,3,4,5]
+# p a.delete_at(3)
+# p a
+
+
+
+
+# new_stack = Stack.new
+
+# if self.value == value
+#     return self
+# elsif self.children
+#     if self.childreen.length == 2
+#         new_stack.push(self.children.last)
+#         new_stack.push(self.children.first)
+#     else
+#         self.children.each do |child|
+#         new_stack.push(child)
+#     end
+# end
+#     # new_stack.push(self.children.last)
+#     # new_stack.push(self.children.first)
+# else
+#     new_stack.pop
+# end
+
+# return nil if new_stack.empty?
+
+# new_var = new_stack.pop
+# new_var.dfs(value)
--- Week6Day1/Knights of Chess/knights.rb
+++ Week6Day1/Knights of Chess/knights.rb
@@ -1,42 +1,154 @@
+require_relative "./00_tree_node"
+
 class KnightPathFinder
+    
+
+    attr_reader :grid, :start_pos
+    attr_accessor :root, :visited_pos
     
     def initialize(start_pos)
         @grid = Array.new(8) { Array.new(8, nil) }
-        @start = @grid[start_pos.first][start_pos.last]
+        @start_pos = start_pos
+        @visited_pos = []
+        @root = PolyTreeNode.new(self.start_pos)
+        # @start = @grid[start_pos.first][start_pos.last]
     end
 
-    def move_a(start)
-        @grid[start.first+2][start.last+1]
-    end
-    def move_b(start)
-        @grid[start.first-2][start.last+1]
-    end
-    def move_c(start)
-        @grid[start.first+2][start.last-1]
-    end
-    def move_d(start)
-        @grid[start.first-2][start.last-1]
+#           A   B    
+
+#      C            D     
+#           start
+#      E            F
+
+#          G    H
+
+
+    def all_moves_from_pos(current_pos)
+        x = current_pos.first
+        y = current_pos.last
+        moves = []
+        new_move = []
+        duo = true
+        nega = false
+        fur = false
+        i = 0
+        while i < 8
+            if duo == true && nega == false && fur == false
+                nega = true
+                moves << [x + 2, y + 1]
+            elsif duo == true && nega == true && fur == false
+                nega = false
+                fur = true
+                moves << [x + 2, y - 1]
+            elsif duo == true && nega == false && fur == true
+                nega = true
+                moves << [x - 2, y + 1]
+            elsif duo == true && nega == true && fur == true
+                nega = false
+                duo = false
+                fur = false
+                moves << [x - 2, y - 1]
+            elsif duo == false && nega == false && fur == false
+                nega = true
+                moves << [x + 1, y + 2]
+            elsif duo == false && nega == true && fur == false
+                nega = false
+                fur = true
+                moves << [x + 1, y - 2]
+            elsif duo == false && nega == false && fur == true
+                nega = true
+                moves << [x - 1, y + 2]
+            else
+                moves << [x - 1, y - 2]
+            end
+            i += 1
+        end
+        moves
     end
 
-    def move_e(start)
-        @grid[start.first+1][start.last+2]
-    end
-    def move_f(start)
-        @grid[start.first-1][start.last+2]
-    end
-    def move_g(start)
-        @grid[start.first+1][start.last-2]
-    end
-    def move_h(start)
-        @grid[start.first-1][start.last-2]
-    end
-
-    def find_path(end_pos)
+    def build_tree(end_pos)
+        new_que = []
         
+        new_que << self.root
+        #return moves_taken if self.start_pos == end_pos
+
+        until new_que.empty?
+            current_node = new_que.shift
+        
+            #return moves_taken << current_node if current_node == end_pos
+            #visited_pos << current_node
+            #real_moves = []
+            real_moves = find_real_moves(current_node.value)
+            real_moves.each do |move|
+                 if is_valid_pos?(move)
+                    new_node = PolyTreeNode.new(move)
+                    current_node.add_child(new_node)
+                    new_que << new_node
+                    #moves_taken << move
+                 end
+            end
+            #return moves_taken << current_node if current_node == end_pos
+            #return visited_pos if current_node == end_pos
+        end
+        nil
+
+        # real_moves.each do |ele|
+        #     return moves_taken << ele if ele == end_pos
+        #     find_path(ele)
+        # end
+    
+    end
+
+    def find_real_moves(start)
+        moves = self.all_moves_from_pos(start)
+        real_moves = moves.select { |el| self.is_valid_pos?(el) && !visited_pos.include?(el) }
+        self.visited_pos += real_moves
+        real_moves
+    end
+
+    def is_valid_pos?(pos)
+        pos.first < 8 && pos.last < 8 && pos.first >= 0 && pos.last >= 0
     end
 
 end
 
+
 kpf = KnightPathFinder.new([0, 0])
-p kpf.find_path([2, 1]) # => [[0, 0], [2, 1]]
-p kpf.find_path([3, 3]) # => [[0, 0], [2, 1], [3, 3]]
+p kpf.build_tree([2, 1]) # => [[0, 0], [2, 1]]
+p kpf.build_tree([3, 3]) # => [[0, 0], [2, 1], [3, 3]]
+
+#p kpf.grid[10][10]
+#p kpf.all_moves_from_pos([0,0])
+
+#p kpf.is_valid_pos?([-1,-6])
+
+#___________________ BFS _______________________
+
+# def bfs(value)
+
+        
+#     new_q = []
+
+#     new_q << self
+
+#     until new_q.empty?
+#         current_node = new_q.shift
+#         return current_node if current_node.value == value
+#         current_node.children.each do |child|
+#             new_q << child
+#         end
+        
+#     end
+#     nil
+
+#     # if self.value == value
+#     #     return self
+#     # end
+# end
+
+
+# 0,0,  2,1,   3,4   (4,2)
+
+#0,0  2,1  1,2 (3,0)
+
+#Its almost like it is printing out a subset of que ... 
