7e4d99538ca411e04ac885d81cf45fc894004831
Tasunke Witka <tmbrasfield01@gmail.com>
2019-11-21 09:29:09 -0600

    Class Inheritance project

--- Week6Day3/Class Inheritance/employee.rb
+++ Week6Day3/Class Inheritance/employee.rb
@@ -1,20 +1,24 @@
 class Employee
 
-    attr_accessor :salary
+    attr_accessor :salary, :employees
     attr_reader :boss
 
-    def initialize(name,title,boss)
+    def initialize(name,title,boss=nil)
         @name = name 
         @salary = 100
         @title = title 
         @boss = boss
         #@bonus = 1
+        @employees = []
     end
 
     def bonus(multiplier)
       bonus = self.salary * multiplier
     end
 
+    def boss=(boss)
+        @boss = boss
+    end
     # def employee(subordinate)
     # end
 
--- Week6Day3/Class Inheritance/manager.rb
+++ Week6Day3/Class Inheritance/manager.rb
@@ -2,15 +2,25 @@ require_relative "employee"
 require "byebug"
 class Manager < Employee
 
+
+    # Founder
+    # |     \
+    # CEO     CFO
+    # |  \       |   \ 
+    # MA  MB     MC   MD
+
     attr_accessor :bonus, :employees
     attr_reader :name, :title, :salary, :boss
 
-     def initialize(name,title,boss,favorite_employee)
+
+
+     def initialize(name,title,boss=nil,favorite_employee)
 
         super(name, title, boss)
 
         @employees = []
         @favorite_employee = favorite_employee
+
     end
     #debugger
 
@@ -21,11 +31,37 @@ class Manager < Employee
     
     def bonus(multiplier)
         x = 0 
-        @employees.each do |worker| 
-            x += worker.salary
+        @employees.each.salary do |worker| 
+            worker.emplyees.each do |sub|
+                x += sub_salary(sub)
+            end
         end
-        @bonus = x * multiplier
+        @bonus = x.sum * multiplier
     end
+
+
+    def sub_salary(worker)
+        workers_salaries = []
+        return workers_salaries.sum if worker.employees.nil?
+        
+        worker.employees.each do |w|
+            workers_salaries += sub_salary(w)
+        end
+
+    end
+
+    # def bonus(multiplier)
+    #     x = 0 
+    #     @employees.each do |worker| 
+    #         if worker.employees.length > 0 
+    #             worker.employees.each do |sub_worker|
+    #                 x += sub_worker.salary
+    #             end
+    #         end 
+    #         x += worker.salary
+    #     end
+    #     @bonus = x * multiplier
+    # end
 
 end
 ned = Manager.new("neal", "founder", nil, "bob")
@@ -36,14 +72,20 @@ ned.salary = 10000
 ted = Employee.new("ted","janitor",ned)
 ted.salary = 200
 
-fred = Employee.new("fred","assistant", ned)
+fred = Manager.new("fred","assistant", ned, "bread")
 
-bread = Employee.new("bread","assistant's assistant", fred)
+bread = Manager.new("bread","assistant's assistant", fred, "carrot")
+bread.salary = 10
+
+carrot = Employee.new("carrots", "coffee-man", bread)
+carrot.salary = 100000
 
 ned.add_employee(ted)
 ned.add_employee(fred)
 
+fred.add_employee(bread)
 p ned.bonus(5)
 p ted.bonus(3)
+#p ted.boss
 
 #p bread.boss
